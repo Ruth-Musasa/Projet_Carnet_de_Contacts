5e0abb0eca0ed1c86d4f073b6ad727857eef344e
Glody Mafo <glodymafokis@gmail.com>
2023-11-20 16:42:53 +0200

    image_checking

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 // section image a ne pas supprimer!!
-const img = document.getElementById("image")
-img.style.opacity = 0;
+var img = document.getElementById("image")
+// img.style.opacity = 0;
+img.addEventListener('',img_validation);
 
-// section telephone
 
-const info = document.querySelector(".alert-info");
 
-function process(event) {
-    event.preventDefault();
-
-    const phoneNumber = 1*phoneInput.getNumber();
-    alert(phoneNumber)
-    alert(typeof phoneNumber)
-    if (typeof phoneNumber == "number" || phoneNumber.length == 13) {
-        if ((phoneNumber[3] == 9 || phoneNumber[3] == 9) && (phoneNumber[4] == 2 || phoneNumber[4] == 4 || phoneNumber[4] == 7 || phoneNumber[4] == 8 || phoneNumber[4] == 9)) {
-            info.style.display = "";
-            alert(typeof phoneNumber)
-            info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`
-        }
-    }
-    else {
-        info.style.display = "";
-        info.innerHTML = `ecrivez un bon num <strong></strong>`
-
-    }
-}
 
 //Section Email Checking
 
@@ -42,16 +22,20 @@ function checking(event) {
     let testEmail = emailRegExp.test(email.value);
     let erreur = email.nextElementSibling;
 
-    if (testEmail) {
+    if (email.value.trim()==""){
+        erreur.innerHTML = 'Le champ email est requis'
+        email.classList.add("erreur") 
+    }
+
+   else if (testEmail) {
         erreur.innerHTML = "";
     } else {
         erreur.innerHTML = 'Veuillez saisir une adresse E-mail correcte'
-        email.classList.add("erreur")
+        email.classList.add("erreur")        
     }
 }
 
 
-//Section image checking
 
 
 
