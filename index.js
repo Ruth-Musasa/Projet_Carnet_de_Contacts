7eae50d18d8f000f78dbfdf91a84245b51902923
Ruth Musasa <ruthebondo2001@gmail.com>
2023-11-20 16:45:14 +0200

    ajout phone

--- index.js
+++ index.js
@@ -2,29 +2,6 @@
 const img = document.getElementById("image")
 img.style.opacity = 0;
 
-// section telephone
-
-const info = document.querySelector(".alert-info");
-
-function process(event) {
-    event.preventDefault();
-
-    const phoneNumber = 1*phoneInput.getNumber();
-    alert(phoneNumber)
-    alert(typeof phoneNumber)
-    if (typeof phoneNumber == "number" || phoneNumber.length == 13) {
-        if ((phoneNumber[3] == 9 || phoneNumber[3] == 9) && (phoneNumber[4] == 2 || phoneNumber[4] == 4 || phoneNumber[4] == 7 || phoneNumber[4] == 8 || phoneNumber[4] == 9)) {
-            info.style.display = "";
-            alert(typeof phoneNumber)
-            info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`
-        }
-    }
-    else {
-        info.style.display = "";
-        info.innerHTML = `ecrivez un bon num <strong></strong>`
-
-    }
-}
 
 //Section Email Checking
 
@@ -51,7 +28,27 @@ function checking(event) {
 }
 
 
-//Section image checking
+
+
+// section telephone
+
+const info = document.querySelector("#phone");
+info.addEventListener('change', process);
+function process(event) {
+    event.preventDefault();
+
+    phoneRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+
+    let testPhone = phoneRegExp.test(info.value);
+    let erreur = info.nextElementSibling;
+
+    if (testPhone) {
+        erreur.innerHTML = "";
+    } else {
+        erreur.innerHTML = 'Veuillez saisir un numero correcte'
+        info.classList.add("erreur")
+    }
+}
 
 
 
