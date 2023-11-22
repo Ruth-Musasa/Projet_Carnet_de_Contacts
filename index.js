8400ebed352459478d3e31760b5ab67eb6eef2ae
ruben_dev <lubasaruben44@gmail.com>
2023-11-22 17:17:57 +0200

    modif de fin

--- index.js
+++ index.js
@@ -39,22 +39,20 @@ function process(event) {
     let erreur = info.nextElementSibling;
     if (testPhone) {
 
-// function process(event) {
-//     event.preventDefault();
+            erreur.innerHTML = "";
+            info.style.border = "1px solid rgb(179, 177, 177)";
+    }
+    else if (info.value.length!==10) {
+            erreur.innerHTML = 'Veuillez saisir un numero correcte composée de 10 chiffres'
+            info.style.border = "2px solid red"
+        }
+    else {
+        erreur.innerHTML = 'Veuillez saisir un numero correcte'
+        info.style.border = "2px solid red"
+    }
+}
 
-//     const phoneNumber = phoneInput.getNumber();
-//     alert(phoneNumber)
-//     alert(typeof phoneNumber)
-//     if (typeof phoneNumber == "number" || phoneNumber.length == 13) {
-//         if ((phoneNumber[3] == 9 || phoneNumber[3] == 9) && (phoneNumber[4] == 2 || phoneNumber[4] == 4 || phoneNumber[4] == 7 || phoneNumber[4] == 8 || phoneNumber[4] == 9)) {
-//             info.style.display = "";
-//             alert(typeof phoneNumber)
-//             info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`
-//         }
-//     }
-//     else {
-//         info.style.display = "";
-//         info.innerHTML = `ecrivez un bon num <strong></strong>`
+//Section Email Checking
 
 let email = document.querySelector('#email_checking');
 
