2e3991ff6e500f66b6335ab45e568e091a2a5534
Ruth Musasa <ruthebondo2001@gmail.com>
2023-11-28 14:50:31 +0200

    affichage image

--- index.js
+++ index.js
@@ -111,15 +111,27 @@ img.addEventListener('change', imgchecking);
 
 function imgchecking(event) {
     event.preventDefault();
-    imgRegExp = new RegExp('^(.+[\.]{1}(jpg|png|jpeg))$', 'g');
-    let testimg = imgRegExp.test(img.value);
+    imgRegExp = new RegExp('^(.+/(jpg|png|jpeg))$', 'g');
+    let testimg = imgRegExp.test(this.files[0].type);
     let erreur = img.nextElementSibling;
+    let tabImg = [Obj = {
 
-    if (testimg == true) {
+    }
+    ];
+    if (testimg) {
         if (this.files[0].size <= 5000000) {
             erreur.innerHTML = "";
-            imgStyle.style.border = "2px dashed #D5E9E1"; 
-            imgStyle.style.backgroundColor = "#D5E9E1"
+            imgStyle.style.border = "2px dashed #D5E9E1";
+            imgStyle.style.backgroundColor = "#D5E9E1";
+            let i;
+
+            for (i = 0; i < tabImg.length; i++) {
+                tabImg = tabImg + this.files[i]
+                console.log(tabImg);
+                return tabImg
+            }
+            showFile(this.files[i]);
+            return true
         } else {
             erreur.innerHTML = 'Deposer une image de moins de 5Mo'
             imgStyle.style.border = "2px dashed red"
@@ -133,6 +145,19 @@ function imgchecking(event) {
     }
 }
 
+function showFile(file) {
+    // let img =document.createElement('div')
+    // img.classList.add("size_img")
+    // img.appendChild(contenaireListe)
+    let dropzone = document.querySelector('#size_img');
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+        let fileUrl = fileReader.result
+        dropzone.innerHTML = `<img src="${fileUrl}" alt = "image"/>`;
+    }
+
+};
 // section boutton
 
 let contacts;
@@ -145,31 +170,30 @@ boutton.onclick = function () {
     contacts.groupe = groupe.value;
     contacts.email = email.value;
     contacts.bio = bio.value;
-    contacts.img =
-        console.log(contacts);
-
-};
-
-let contenaireListe;
-function saveData() {
-    contenaireListe = document.querySelector(".contenaire--liste");
-    localStorage.setItem("data", contenaireListe.innerHTML);
+    // contacts.img =newImg;
     console.log(contacts);
 };
+console.log("r");
+// let contenaireListe;
+// function saveData() {
+//     contenaireListe = document.querySelector(".contenaire--liste");
+//     localStorage.setItem("data", contenaireListe.innerHTML);
+//     console.log(contacts);
+// };
 
-function showTask() {
-    contenaireListe.innerHTML = localStorage.getItem("data");
-}
+// function showTask() {
+//     contenaireListe.innerHTML = localStorage.getItem("data");
+// }
 
-showTask();
+// showTask();
 
-let newImg = document.createElement('img');
-newImg.id = 'size_img';
-newImg.src = 'img.value';
-newImg.alt = 'Photo id';
-document.getElementsByClassName('.contenaire--liste').appendChild(newImg);
-newImg.innerHTML = newImg.src
 
-// document.body.appendChild(span);
-span.innerHTML = 'Du texte en plus !';
-console.log(newImg);
+
+// newImg.id = 'size_img';
+// newImg.src = 'img.value';
+// newImg.alt = 'Photo id';
+// document.getElementsByClassName('.contenaire--liste').appendChild(newImg);
+// newImg.innerHTML = newImg.src
+
+// span.innerHTML = 'Du texte en plus !';
+// console.log(newImg);
