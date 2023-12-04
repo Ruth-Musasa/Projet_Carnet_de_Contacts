7ea3675152f020cb72bbc71fc44404eaec398a60
Ruth Musasa <ruthebondo2001@gmail.com>
2023-12-04 11:24:21 +0200

    review code ruth

--- index.js
+++ index.js
@@ -1,102 +1,91 @@
-
-//Section prénom checking
-
+//Section prénom validationEmail
 let prénom = document.querySelector("#nameInpute");
-prénom.addEventListener('change', validateForm)
-
-function validateForm(event) {
+prénom.addEventListener('change', validationPrenom)
+function validationPrenom(event) {
     event.preventDefault();
-    nameRegExp = new RegExp('^[a-zA-Z]$', 'g');
-    testname = nameRegExp.test(nom.value);
-
+    nameRegExp = new RegExp('^[a-zA-Z0-9]{3,50}$', 'g');
+    testname = nameRegExp.test(prénom.value);
     let erreur = prénom.nextElementSibling;
-
-    if (prénom.value.length < 3) {
-        erreur.innerHTML = "Veuillez saisir un prénom valide d'au moins 3 caractères"
-        prénom.style.border = "2px solid red"
-        return false;
-    }
-    else if (prénom.value.length > 50) {
-        erreur.innerHTML = 'Veuillez saisir un prénom valide inférieur à 50 caractères'
-        prénom.style.border = "2px solid red"
-        return false;
-    }
-    else {
+    if (testname) {
         erreur.innerHTML = "";
         prénom.style.border = "1px solid rgb(179, 177, 177)"
         return true;
+    } else if (prénom.value.length < 3) {
+        erreur.innerHTML = "Veuillez saisir un prénom valide d'au moins 3 caractères"
+        prénom.style.border = "2px solid red"
+        return false;
+    } else if (prénom.value.length > 50) {
+        erreur.innerHTML = 'Veuillez saisir un prénom valide inférieur à 50 caractères'
+        prénom.style.border = "2px solid red"
+        return false;
+    } else {
+        erreur.innerHTML = 'Veuillez saisir un prénom valide composé de lettres'
+        prénom.style.border = "2px solid red"
+        return false;
     }
 }
 
-//Section nom checking
-
+//Section nom validationEmail
 let nom = document.querySelector("#nom");
 nom.addEventListener('change', validateName)
-
 function validateName(event) {
     event.preventDefault();
-    nameRegExp = new RegExp('^[a-zA-Z]$', 'g');
+    nameRegExp = new RegExp('^[a-zA-Z0-9]{3,50}$', 'g');
     testname = nameRegExp.test(nom.value);
     let erreur = nom.nextElementSibling;
-
-    if (nom.value.length < 3) {
-        erreur.innerHTML = "Veuillez saisir un nom valide d'au moins 3 caractères"
-        nom.style.border = "2px solid red"
-        return false;
-    }
-    else if (nom.value.length > 50) {
-        erreur.innerHTML = 'Veuillez saisir un nom valide inférieur à 50 caractères'
-        nom.style.border = "2px solid red"
-        return false;
-    }
-    else {
+    if (testname) {
         erreur.innerHTML = "";
         nom.style.border = "1px solid rgb(179, 177, 177)"
         return true;
+    } else if (nom.value.length < 3) {
+        erreur.innerHTML = "Veuillez saisir un nom valide d'au moins 3 caractères"
+        nom.style.border = "2px solid red"
+        return false;
+    } else if (nom.value.length > 50) {
+        erreur.innerHTML = 'Veuillez saisir un nom valide inférieur à 50 caractères'
+        nom.style.border = "2px solid red"
+        return false;
+    } else {
+        erreur.innerHTML = 'Veuillez saisir un prénom valide composé de lettres'
+        nom.style.border = "2px solid red"
+        return false;
     }
 }
 
 // section telephone
-
-var info = document.querySelector("#phone");
-info.addEventListener('change', process);
-
-function process(event) {
+var phone = document.querySelector("#phone");
+phone.addEventListener('change', validationPhone);
+function validationPhone(event) {
     event.preventDefault();
     phoneRegExp = new RegExp('^(084|085|080|089|081|082|099|097|090)([0-9]{7})$', 'g');
-    testPhone = phoneRegExp.test(info.value);
-    let erreur = info.nextElementSibling;
-
+    testPhone = phoneRegExp.test(phone.value);
+    let erreur = phone.nextElementSibling;
     if (testPhone) {
         erreur.innerHTML = "";
-        info.style.border = "1px solid rgb(179, 177, 177)";
+        phone.style.border = "1px solid rgb(179, 177, 177)";
         return true
-    }
-    else if (info.value.length !== 10) {
+    } else if (phone.value.length !== 10) {
         erreur.innerHTML = 'Veuillez saisir un numero correcte composée de 10 chiffres'
-        info.style.border = "2px solid red"
+        phone.style.border = "2px solid red"
         return false
-    }
-    else {
-        erreur.innerHTML = 'Veuillez saisir un numero correcte'
-        info.style.border = "2px solid red"
+    } else {
+        erreur.innerHTML = 'Veuillez saisir un numero correcte respectant les formats (Orange : 084,085,080,089, Vodacom: 081,082, Airtel : 099,097, Africell: 090)'
+        phone.style.border = "2px solid red"
         return false
     }
 }
+
 // section groupe
 let groupe = document.querySelector('#groupe');
 
-//Section Email Checking
-
+//Section Email validationEmail
 let email = document.querySelector('#email_checking');
-email.addEventListener('change', checking);
-
-function checking(event) {
+email.addEventListener('change', validationEmail);
+function validationEmail(event) {
     event.preventDefault();
     emailRegExp = new RegExp('^[a-zA-Z0-9]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]{2,10}$', 'g');
     testEmail = emailRegExp.test(email.value);
     let erreur = email.nextElementSibling;
-
     if (testEmail) {
         erreur.innerHTML = "";
         email.style.border = "1px solid rgb(179, 177, 177)"
@@ -116,14 +105,11 @@ let testimg;
 let imgStyle = document.querySelector('#inputImg_height')
 let img = document.querySelector('#image');
 img.style.opacity = 0;
-img.addEventListener('change', imgchecking);
-
-function imgchecking() {
+img.addEventListener('change', imgvalidation);
+function imgvalidation() {
     imgRegExp = new RegExp('^.+/(jpg|png|jpeg)$', 'g');
     testimg = imgRegExp.test(this.files[0].type);
-
     let erreur = img.nextElementSibling;
-
     if (testimg) {
         if (this.files[0].size <= 5000000) {
             erreur.innerHTML = "";
@@ -146,34 +132,29 @@ function imgchecking() {
 }
 let fileUrl;
 function showFile() {
-    // let dropzone = document.querySelector('#size_img');
     let fileReader = new FileReader();
     fileReader.readAsDataURL(img.files[0]);
     fileReader.onload = () => {
         fileUrl = fileReader.result
-        // dropzone.innerHTML = `<img src="${fileUrl}" alt = "image"/>`;
+        return true
     }
-
 }
 
+// creation de l'objet contact
 let objContact
 let listContact = [];
 function objectaff() {
-
     objContact = {
         prenom: prénom.value,
         Nom: nom.value,
-        telephone: info.value,
+        telephone: phone.value,
         Groupe: groupe.value,
         Email: email.value,
         Bio: bio.value,
-
     }
     listContact.push(objContact);
     return objContact
 }
-
-
 
 //********************VERIFICATION DU FORMULAIRE ET VALIDATION DU BOUTON****************************** */
 
@@ -182,41 +163,30 @@ let button = document.querySelector("#button_color--blue");
 
 //Vérification du formulaire : la fonction gère les couleurs du bouton creer en fonction de la validité des champs du formulaire (A NE PAS MODIFIER)
 
-form.addEventListener('input', formChecking);
+form.addEventListener('input', formvalidation);
 
-
-function formChecking(event) {
-
+function formvalidation(event) {
     event.preventDefault();
-
-    if (validateForm(event) && validateName(event) && process(event) && groupe.value != "" && checking(event) && bio.value != "") {
+    if (validationPrenom(event) && validateName(event) && validationPhone(event) && groupe.value != "" && validationEmail(event) && bio.value != "" ) {
         objectaff();
         showFile()
-        console.log(objectaff());
-
         button.style.backgroundColor = "rgb(8, 128, 214)";
-    }
-    else {
-
+    } else {
         button.style.backgroundColor = "rgb(85, 85, 85)";
     }
-
 }
 
 //Validation du bouton creer
 
 button.addEventListener('click', function (event) {
     event.preventDefault();
-    imgchecking.call(img);
+    imgvalidation.call(img);
     execution(event)
 });
 
-
 function execution(event) {
     event.preventDefault();
-
-    if (validateForm(event) && validateName(event) && process(event) && groupe.value != "" && checking(event) && bio.value != "") {
-
+    if (validationPrenom(event) && validateName(event) && validationPhone(event) && groupe.value != "" && validationEmail(event) && bio.value != "" ) {
 
         //PLACEZ LA FONCTION QUI AJOUTE ET AFFICHE LES COORDONNEES ICI 
 
@@ -224,14 +194,14 @@ function execution(event) {
         const div = document.createElement("div");
         affichageListe.appendChild(div);
         div.classList.add('contenaire--liste');
-        div.innerHTML = 
-`<div class="contenaire--list--space">
+        div.innerHTML =
+            `<div class="contenaire--list--space">
     <div id="size_img"><img src="${fileUrl}" alt = "image"/></div>
     <div>
         <div id="contenaire--liste--id">
-            <p id="affichage_Prénom">${objContact.prenom} </p>
-            <p id="affichage_Nom">${objContact.Nom}</p>
-            <p id="affichage_Groupe">- ${objContact.Groupe} </p> 
+            <p> ${objContact.prenom} </p>
+            <p> ${objContact.Nom} </p>
+            <p> - ${objContact.Groupe} </p> 
         </div>
         <div id="contenaire--liste--num">${objContact.telephone}</div>
         <div id="contenaire--liste--bio">${objContact.Bio}</div>
@@ -239,10 +209,9 @@ function execution(event) {
     <div id="icon">
             <div id="edit_btn"> <img src="icon/Vector.png" alt=""></div>
             <div id="delet_btn"> <img src="icon/VectorSupp.png" alt=""></div>
-    </div>
+        </div>
 </div>`
         button.style.backgroundColor = "rgb(8, 128, 214)"
-
     }
     showFile();
 }
