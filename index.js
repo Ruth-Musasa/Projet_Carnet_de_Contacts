7c79bb65d089f57242caabd15ce42b6faae56a39
ruben_dev <lubasaruben44@gmail.com>
2023-12-06 16:17:57 +0200

    du jour

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //Section prénom checking
 
 let prénom = document.querySelector("#nameInpute");
@@ -7,24 +6,20 @@ prénom.addEventListener('change', validateForm)
 function validateForm(event) {
     event.preventDefault();
     nameRegExp = new RegExp('^[a-zA-Z]$', 'g');
-    testname = nameRegExp.test(nom.value);
-
+    let testname = nameRegExp.test(prénom.value);
     let erreur = prénom.nextElementSibling;
 
     if (prénom.value.length < 3) {
         erreur.innerHTML = "Veuillez saisir un prénom valide d'au moins 3 caractères"
         prénom.style.border = "2px solid red"
-        return false;
     }
     else if (prénom.value.length > 50) {
         erreur.innerHTML = 'Veuillez saisir un prénom valide inférieur à 50 caractères'
         prénom.style.border = "2px solid red"
-        return false;
     }
-    else {
+    else if (testname) {
         erreur.innerHTML = "";
         prénom.style.border = "1px solid rgb(179, 177, 177)"
-        return true;
     }
 }
 
@@ -36,23 +31,20 @@ nom.addEventListener('change', validateName)
 function validateName(event) {
     event.preventDefault();
     nameRegExp = new RegExp('^[a-zA-Z]$', 'g');
-    testname = nameRegExp.test(nom.value);
+    let testname = nameRegExp.test(nom.value);
     let erreur = nom.nextElementSibling;
 
     if (nom.value.length < 3) {
         erreur.innerHTML = "Veuillez saisir un nom valide d'au moins 3 caractères"
         nom.style.border = "2px solid red"
-        return false;
     }
     else if (nom.value.length > 50) {
         erreur.innerHTML = 'Veuillez saisir un nom valide inférieur à 50 caractères'
         nom.style.border = "2px solid red"
-        return false;
     }
-    else {
+    else if (testname) {
         erreur.innerHTML = "";
         nom.style.border = "1px solid rgb(179, 177, 177)"
-        return true;
     }
 }
 
@@ -64,23 +56,20 @@ info.addEventListener('change', process);
 function process(event) {
     event.preventDefault();
     phoneRegExp = new RegExp('^(084|085|080|089|081|082|099|097|090)([0-9]{7})$', 'g');
-    testPhone = phoneRegExp.test(info.value);
+    let testPhone = phoneRegExp.test(info.value);
     let erreur = info.nextElementSibling;
 
     if (testPhone) {
         erreur.innerHTML = "";
         info.style.border = "1px solid rgb(179, 177, 177)";
-        return true
     }
     else if (info.value.length !== 10) {
         erreur.innerHTML = 'Veuillez saisir un numero correcte composée de 10 chiffres'
         info.style.border = "2px solid red"
-        return false
     }
     else {
         erreur.innerHTML = 'Veuillez saisir un numero correcte'
         info.style.border = "2px solid red"
-        return false
     }
 }
 // section groupe
@@ -94,17 +83,19 @@ email.addEventListener('change', checking);
 function checking(event) {
     event.preventDefault();
     emailRegExp = new RegExp('^[a-zA-Z0-9]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]{2,10}$', 'g');
-    testEmail = emailRegExp.test(email.value);
+    let testEmail = emailRegExp.test(email.value);
     let erreur = email.nextElementSibling;
 
-    if (testEmail) {
+    if (email.value.trim() == "") {
+        erreur.innerHTML = 'Le champ Email est requis'
+        email.style.border = "2px solid red"
+    }
+    else if (testEmail) {
         erreur.innerHTML = "";
         email.style.border = "1px solid rgb(179, 177, 177)"
-        return true;
     } else {
         erreur.innerHTML = 'Veuillez saisir une adresse E-mail correcte'
         email.style.border = "2px solid red"
-        return false;
     }
 }
 
@@ -158,15 +149,12 @@ function showFile(file) {
 
 };
 
-// let contenaireListe;
+// PARTIE AFFICHAGE
 
+// Tableau pour stocker les contacts
+const contacts = [];
 
-
-function formChecking(event) {
-
-    event.preventDefault();
-
-    if (validateForm(event) && validateName(event) && process(event) && groupe.value != "" && checking(event) && bio.value != "") {
+// Récupérer les éléments du formulaire
 
 const nomInput = document.getElementById('nom').value;
 const prenomInput = document.getElementById('nameInpute').value;
@@ -174,29 +162,25 @@ const telephoneInput = document.getElementById('phone').value;
 const bioInput = document.getElementById('inputBio_height').value;
 const emailInput = document.getElementById('email_checking').value;
 const creerBouton = document.getElementById('button_color--blue');
+console.log("e");
+// Écouter l'événement de clic sur le bouton "Créer"
+creerBouton.addEventListener('click', add); 
+function add(e)  {
+e.preventDefault()
+    // Créer un objet contact avec les informations
 
-        button.style.backgroundColor = "rgb(8, 128, 214)";
-    }
-    else {
-
-creerBouton.onclick = function() {
-
-    
-// Créer un objet contact avec les informations
-
-const nouveauContact = {
-    nom: nomInput,
-    prenom: prenomInput,
-    telephone: telephoneInput,
-    bio: bioInput,
-    email: emailInput,
-};
-
+    const nouveauContact = {
+        nom: nomInput,
+        prenom: prenomInput,
+        telephone: telephoneInput,
+        bio: bioInput,
+        email: emailInput,
+    };
+    console.log('o');
     // Ajouter le nouvel objet contact au tableau
 
     contacts.push(nouveauContact);
     console.log(nouveauContact);
-
     // Réinitialiser les champs du formulaire
 
     nomInput.value = '';
@@ -206,9 +190,8 @@ const nouveauContact = {
     emailInput.value = '';
 
     // Afficher les contacts dans la liste de contacts
-
     afficherContacts();
-};
+} ;
 
 // Fonction pour afficher les contacts dans la liste de contacts
 
@@ -224,10 +207,61 @@ function afficherContacts() {
 
     // Parcourir le tableau de contacts et créer un élément de liste pour chaque contact
 
-    contacts.forEach(function (contact) {
+    contacts.forEach(function (contacts) {
         const nouvelElement = document.createElement('li');
-        nouvelElement.innerHTML = `Nom: ${contact.nom}, Prénom: ${contact.prenom}, Téléphone: ${contact.telephone}, Bio: ${contact.bio}, Email: ${contact.email}`;
+        nouvelElement.innerHTML = `Nom: ${contacts.nom}, Prénom: ${contacts.prenom}, Téléphone: ${contacts.telephone}, Bio: ${contacts.bio}, Email: ${contacts.email}`;
         listeContacts.appendChild(nouvelElement);
+        console.log("j");
     });
- }
+}
+// console.log(contacts);
 
+
+
+// function ajouterContact(event) {
+//     event.preventDefault();
+  
+// const nomInput = document.getElementById('nom').value;
+// const prenomInput = document.getElementById('nameInpute').value;
+// const telephoneInput = document.getElementById('phone').value;
+// const bioInput = document.getElementById('inputBio_height').value;
+// const emailInput = document.getElementById('email_checking').value;
+// // const creerBouton = document.getElementById('button_color--blue');
+  
+//     const nouveauContact = `
+//       <li>
+//         <span>${nomInput} ${prenomInput}</span>
+//         <span>${emailInput}</span>
+//         <span>${telephoneInput}</span>
+//         <span>${bioInput}</span>
+//         <button class="delete-btn">Supprimer</button>
+//       </li>
+//     `;
+  
+//     document.getElementById('contenaire--formulaire').innerHTML += nouveauContact;
+  
+//     // Réinitialisation du formulaire après l'ajout du contact
+//     document.getElementById('contenaire--formulaire--marges').reset();
+  
+//     // Attache d'événement pour la suppression de contact
+//     attacherSuppression();
+//   }
+  
+//   // Fonction pour supprimer un contact de la liste
+//   function supprimerContact(event) {
+//     if (event.target.classList.contains('.delete-btn')) {
+//       event.target.parentElement.remove();
+//     }
+//   }
+  
+//   // Fonction pour attacher l'événement de suppression à chaque bouton
+//   function attacherSuppression() {
+//     const deleteButtons = document.querySelector('.delete-btn');
+//     deleteButtons.forEach(button => {
+//       button.addEventListener('click', supprimerContact);
+//     });
+//   }
+  
+//   // Attacher l'événement submit pour ajouter un contact
+//   document.querySelector('contenaire--formulaire--marges').addEventListener('submit', ajouterContact);
+  
